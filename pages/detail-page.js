e90d57856fb10ab03151db45996509a2394151d8
erdalaydin <erdalaydin92@hotmail.com>
2019-08-07 10:50:02 +0200

    Create elements with data from form

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,3 +1,18 @@
-function submitComment(){
-    console.log('Hello')
-}
+// add event handler
+function submitComment() {
+    // ...
+    
+    // create the elements you need
+    const comment = document.createElement('section')
+    const h3 = document.createElement('h3')
+    const p = document.createElement('p')
+  
+    // adjust the elements we created
+    h3.innerHTML = `${name} said:`
+    p.innerHTML = msg
+    comment.classList.add('comment')
+    comment.appendChild(h3)
+    comment.appendChild(p)
+  
+    console.log(comment)
+  }
