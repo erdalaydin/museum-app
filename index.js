5c5017b2d38913c3261cc768470cb512d6bb73c7
erdalaydin <erdalaydin92@hotmail.com>
2019-08-08 14:08:02 +0200

    Set up js files and declare submitComment

---
+++ index.js
@@ -0,0 +1,196 @@
+
+const data = {
+  elapsedMilliseconds: 0,
+  count: 359,
+  countFacets: {
+    hasimage: 357,
+    ondisplay: 43
+  },
+  artObjects: [
+    {
+      links: {
+        self: "https://www.rijksmuseum.nl/api/nl/collection/SK-A-180",
+        web: "https://www.rijksmuseum.nl/nl/collectie/SK-A-180"
+      },
+      id: "nl-SK-A-180",
+      objectNumber: "SK-A-180",
+      title: "Een vrolijke vioolspeler",
+      hasImage: true,
+      principalOrFirstMaker: "Gerard van Honthorst",
+      longTitle: "Een vrolijke vioolspeler, Gerard van Honthorst, 1623",
+      showImage: false,
+      permitDownload: false,
+      webImage: {
+        guid: "868fdc90-94e1-432c-92aa-bc328a866119",
+        offsetPercentageX: 0,
+        offsetPercentageY: 0,
+        width: 2500,
+        height: 2500,
+        url: "https://gazetekarinca.com/wp-content/uploads/2019/05/k%C3%BCrt_%C3%A7acda%C5%9F_sanat.jpg"
+      },
+      headerImage: {
+        guid: "03e0bed7-56ce-48f4-828a-6a3c96f578f1",
+        offsetPercentageX: 0,
+        offsetPercentageY: 0,
+        width: 460,
+        height: 460,
+        url: "https://lh3.googleusercontent.com/lqio6vHMqpMaVBeWBHUxhqpgnyYsTG24Xf-6o2JzuRscjt5TphP3DKoDX-M8VnrDAtnyEI_ROpnSVvttlGWba_3pfwo"
+      },
+      productionPlaces: [ ]
+      },
+      {
+        links: {
+          self: "https://www.rijksmuseum.nl/api/nl/collection/SK-A-1505",
+          web: "https://www.rijksmuseum.nl/nl/collectie/SK-A-1505"
+        },
+        id: "nl-SK-A-1505",
+        objectNumber: "SK-A-1505",
+        title: "Een molen aan een poldervaart, bekend als ‘In de maand juli’",
+        hasImage: true,
+        principalOrFirstMaker: "Paul Joseph Constantin Gabriël",
+        longTitle: "Een molen aan een poldervaart, bekend als ‘In de maand juli’, Paul Joseph Constantin Gabriël, ca. 1889",
+        showImage: false,
+        permitDownload: false,
+        webImage: {
+          guid: "3e83eca2-5306-4d6d-b499-f86091c81d39",
+          offsetPercentageX: 0,
+          offsetPercentageY: 0,
+          width: 2748,
+          height: 2748,
+          url: "https://render.fineartamerica.com/images/rendered/default/print/8.000/5.375/break/images-medium-5/kurdish-refugees-motionage-art-and-design--ahmet-asar.jpg"
+        },
+        headerImage: {
+          guid: "34e94d7f-4d7a-464e-b3f9-eb5532f98c27",
+          offsetPercentageX: 0,
+          offsetPercentageY: 0,
+          width: 424,
+          height: 424,
+          url: "https://lh3.googleusercontent.com/aBYq1zHv15e7b3QhO20l8calrdCVOljfAjpoZeipAFZi53VhuDD-CFOym2ICOWD10C79flH_nW7rU7XBOQCKq50ozlY"
+        },
+        productionPlaces: [ ]
+      },
+      {
+        links: {
+          self: "https://www.rijksmuseum.nl/api/nl/collection/SK-C-5",
+          web: "https://www.rijksmuseum.nl/nl/collectie/SK-C-5"
+        },
+        id: "nl-SK-C-5",
+        objectNumber: "SK-C-5",
+        title: "Schutters van wijk II onder leiding van kapitein Frans Banninck Cocq, bekend als de ‘Nachtwacht’",
+        hasImage: true,
+        principalOrFirstMaker: "Rembrandt van Rijn",
+        longTitle: "Schutters van wijk II onder leiding van kapitein Frans Banninck Cocq, bekend als de ‘Nachtwacht’, Rembrandt van Rijn, 1642",
+        showImage: false,
+        permitDownload: false,
+        webImage: {
+          guid: "aa08df9c-0af9-4195-b31b-f578fbe0a4c9",
+          offsetPercentageX: 0,
+          offsetPercentageY: 1,
+          width: 2034,
+          height: 2034,
+          url: "https://cdn-5b587543f911c8101c180c24.closte.com/wp-content/uploads/2019/05/Untitled-1-1.jpg"
+        },
+        headerImage: {
+          guid: "29a2a516-f1d2-4713-9cbd-7a4458026057",
+          offsetPercentageX: 0,
+          offsetPercentageY: 0,
+          width: 460,
+          height: 460,
+          url: "https://lh3.googleusercontent.com/O7ES8hCeygPDvHSob5Yl4bPIRGA58EoCM-ouQYN6CYBw5jlELVqk2tLkHF5C45JJj-5QBqF6cA6zUfS66PUhQamHAw"
+        },
+        productionPlaces: [
+          "Amsterdam"
+        ]
+      },
+      {
+        links: {
+          self: "https://www.rijksmuseum.nl/api/nl/collection/SK-A-2525",
+          web: "https://www.rijksmuseum.nl/nl/collectie/SK-A-2525"
+        },
+        id: "nl-SK-A-2525",
+        objectNumber: "SK-A-2525",
+        title: "Eenden",
+        hasImage: true,
+        principalOrFirstMaker: "Willem Maris",
+        longTitle: "Eenden, Willem Maris, ca. 1880 - ca. 1904",
+        showImage: false,
+        permitDownload: false,
+        webImage: {
+          guid: "dc93772d-0ac5-44b1-b4f4-c54f450a4ea3",
+          offsetPercentageX: 0,
+          offsetPercentageY: 0,
+          width: 2388,
+          height: 2388,
+          url: "https://i.redd.it/wjqxfsgk0w2z.jpg"
+        },
+        headerImage: {
+          guid: "85aa2151-94c5-44d0-b702-1efe63d82f9d",
+          offsetPercentageX: 0,
+          offsetPercentageY: 0,
+          width: 460,
+          height: 460,
+          url: "https://lh3.googleusercontent.com/hzZqwP-Xb-Z_HckzjZKgBgijjWJmE0XpkB_9EjLw2m-P-fK3OudkzCOvL9w2rRZuWOJ0kKkFU_SBkJ1oSPi9aenV7SE"
+        },
+        productionPlaces: [ ]
+      },
+      {
+        links: {
+          self: "https://www.rijksmuseum.nl/api/nl/collection/SK-A-317",
+          web: "https://www.rijksmuseum.nl/nl/collectie/SK-A-317"
+        },
+        id: "nl-SK-A-317",
+        objectNumber: "SK-A-317",
+        title: "Orpheus en de dieren",
+        hasImage: true,
+        principalOrFirstMaker: "Paulus Potter",
+        longTitle: "Orpheus en de dieren, Paulus Potter, 1650",
+        showImage: false,
+        permitDownload: false,
+        webImage: {
+          guid: "c9abfd61-bbc0-49a2-8d96-b77484315431",
+          offsetPercentageX: 0,
+          offsetPercentageY: 0,
+          width: 2078,
+          height: 2078,
+          url: "https://thumbs.imagekind.com/5520857_650/Kurdish-Lady-waiting-for-her-Hero_art.jpg"
+        },
+        headerImage: {
+          guid: "70f867e3-df27-469f-988a-8759649deebd",
+          offsetPercentageX: 0,
+          offsetPercentageY: 0,
+          width: 460,
+          height: 460,
+          url: "https://lh3.googleusercontent.com/vzaPRVvRQ_miwU-kVn6xOnROQKPLXCm5kBfoY_brYOYbv7WtxkenZRARSXHBpsTdEn5fSLL4n5p7yE0Z4dXQMvBaPeed"
+        },
+        productionPlaces: [ ]
+      },
+      
+  ]
+}
+
+function displayPainting(painting){
+  const title = painting.title
+  const src = painting.webImage.url
+  const gallery = document.getElementById('gallery')
+
+  const a = document.createElement('a')
+  const img = document.createElement('img')
+
+  a.href = './pages/detail-page.html'
+  img.src = src
+  img.alt = title
+  img.classList.add('artObject')
+
+  a.appendChild(img)
+  gallery.appendChild(a)
+}
+
+function displayAllPaintings(paintings) {
+  for (let i = 0; i < paintings.length; i++) {
+    displayPainting(paintings[i])
+  }
+}
+
+const paintings = data.artObjects
+
+displayAllPaintings(paintings)
--- pages/detail-page.js
+++ pages/detail-page.js
@@ -19,6 +19,11 @@ function submitComment() {
     comment.appendChild(p)
   
    
+   if(msg.includes("badwords")){
+     alert("Hey! don't do that!")
+     return; true
+   }
+
     const commentSection = document.getElementById('comments')
     commentSection.appendChild(comment)
   
@@ -26,4 +31,19 @@ function submitComment() {
     inputField.value = null;
     textArea.value = null;
     
+
+    // const paintings = {
+    //   artObjects: [
+    //     { links: {
+    //       self: "https://gazetekarinca.com/wp-content/uploads/2019/05/k%C3%BCrt_%C3%A7acda%C5%9F_sanat.jpg",
+    //       web: "https://gazetekarinca.com/wp-content/uploads/2019/05/k%C3%BCrt_%C3%A7acda%C5%9F_sanat.jpg"
+    //     }
+    //     id: "201905",
+    //     objectNumber: "photo201905",
+    //     title: "A Lady who miss love",
+    //     hasImage: true,      
+
+    //     }
+    //   ]
+    // }
   }
